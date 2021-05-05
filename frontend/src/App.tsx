a656b1468628d9dc49de139cf2af683c2d216d19
Carlos Eduardo <carlosedu9090466@gmail.com>
2021-05-05 15:16:59 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,26 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    // Fragment da function para colocar mais components
+    <>
+      {/* Primeiro vem a navBar e depois o Body */}
+      <NavBar />
+
+      <div className="container">
+
+        <h1 className="text-primary">Olá mundo!</h1>
+
+        <DataTable />
+
+      </div>
+      {/* Footer da aplicação */}
+
+      <Footer />
+
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/carlosedu9090466" target="_blank" rel="noreferrer">Carlos Eduardo</a></p>
+                <p className="text-light"><small><strong>Aprendendo na semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
